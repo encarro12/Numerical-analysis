483549f0ee8a1c3b9aeab68d463ab7bf1792d7a3
Enrique Carro Garrido <100486003+encarro12@users.noreply.github.com>
2023-02-16 09:46:50 +0100

    Lesson of 16/02

--- source/Práctica 2/graficas.m
+++ source/Práctica 2/graficas.m
@@ -1,28 +1,31 @@
-% Si la ecuación diferencial es escalar se dibuja la gráfica de la
-% solución, de color rojo
-if size(x) == 1
-    plot(t,x,'r')
-    legend('x(t)')
-    title('EDO escalar')
-elseif size(x) == 2
-    subplot(2,1,1)
-    plot(t,x(:,1))
-    subplot(2,1,2)
-    plot(t,x(:,2))
-    legend('x1(t), x2(t)')
-    title('EDO R2')
-    pause(5)
-    plot(x(:,1),x(:,2))
-else
-    subplot(3,1,1)
-    plot(t,x(:,1),'r')
-    subplot(3,1,2)
-    plot(t,x(:,2),'g')
-    subplot(3,1,3)
-    plot(t,x(:,3),'b')
-    legend('x1(t), x2(t), x3(t)')
-    title('EDO R3')
-    pause(5)
-    plot(x(:,1),x(:,2),x(:,3))
+%dibuja la gráfica de soluciones tras obtener el vector t y la matriz x de la
+%aproximación de la solución aplicando la función ode45 de Matlab
+
+
+dim = size(x,2);
+color = ['r','g','b'];
+%dibujamos las componentes en diferentes subplots para diferentes posibles
+%dims
+figure(1)
+for i=1:dim
+    subplot(dim,1,i)
+    plot(t,x(:,i),color(i))
+    %sprintf nos permite añadir valor de variables a texto (no como en
+    %disp)
+    s = sprintf('Coordenada %d de la solución',i);
+    title(s)
 end
-    
+
+%a continuación distinguimos casos para dibujar trayectorias en caso de
+%dimensión 2 o 3 (variables frente a otras)
+
+if dim > 1
+    pause(3) %para que de tiempo a visualizar la primera figura
+    figure(2)
+    if (dim == 2)
+        plot(x(:,1),x(:,2),'r')
+    elseif (dim == 3)
+        plot3(x(:,1),x(:,2),x(:,3),'r')
+    end
+    title('Trayectoria de la solución')
+end
