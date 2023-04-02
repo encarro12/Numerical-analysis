509b95a695bf06d4509da0074de974f628271ff4
encarro12 <ecarrogarrido12@gmail.com>
2023-04-02 12:55:52 +0200

    Milne(P) BDF(C) corrected

--- Práctica 4/mmilne4bdf5.m
+++ Práctica 4/mmilne4bdf5.m
@@ -1,6 +1,6 @@
-function [t,x] = mab4am4(f,intervalo, x0, N)
+function [t,x] = mmilne4bdf5(f,intervalo, x0, N)
 % Aquí introducimos las variables que se han de cambiar
-% cuando variamos los métodos de corrección predicción o 
+% cuando variamos los métodos de predicción-corrección o 
 % cuando queremos aumentar el número de correcciones
 % Número de iteraciones (correciones)
 m = 1;
@@ -12,8 +12,13 @@ met = @mrk4;
 % como métodos lineales de r pasos
 beta_pred = (4/3) * [0, 0, 2, -1, 2];
 alpha_corr = (1/137) * [12, -75, 200, -300, 300];
+% De hecho se podrían poner también alpha_pred y beta_corr
+% Lo que pasa que serían vectores prácticamente nulos y en 
+% términos de eficiencia se empeoraría el coste. Esto vale
+% siempre y cuando los métodos de predicción-corrección sean 
+% métodos multipaso lineales.
 
-% INICIALIZACIÓN: Método de Euler Mejorado
+% INICIALIZACIÓN
 h = (intervalo(2) - intervalo(1))/N;
 t = intervalo(1):h:intervalo(2);
 [~, x] = met(f, [t(1), t(r)], x0, r - 1);
@@ -28,10 +33,10 @@ end
 % BUCLE
 for i = 1:N-r+1
     % Predicción (P)
-    x(:,i+r) = x(:,i+r-1) + h*y*(beta_pred.');
+    x(:,i+r) = x(:,i+1) + h*y*(beta_pred.');
     % Evaluación-Corrección (EC)**m
     for j = 1:m
-        x(:,i+r) = x(:,i:i+r-1)*(alpha_corr.') + 60*h*f(t(i+r),x(:,i+r));
+        x(:,i+r) = x(:,i:i+r-1)*(alpha_corr.') + (60/137)*h*f(t(i+r),x(:,i+r));
     end
     % Evaluación
     y = [y(:,2:r), f(t(i+r), x(:,i+r))];
